bab091ad974373c5f5acbd458f99db67fb3067c3
Carlos Alberto Palacios Aguado <cpalacag@emeal.nttdata.com>
2021-11-22 16:54:00 -0500

    Segundo Commit

--- src/main/java/com/nttdata/model/Transaction.java
+++ src/main/java/com/nttdata/model/Transaction.java
@@ -10,13 +10,11 @@ import com.nttdata.model.Transaction;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import lombok.Builder;
 import lombok.Data;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Document(collection = "transaction")
 @Data
-@Builder
 public class Transaction {
 	
 	@Id
@@ -27,18 +25,15 @@ public class Transaction {
 
 	@Field(name = "transactionAmount")
 	private Double transactionAmount;
-
-	@Field(name = "account")
-	private Account account;
-
-	@Field(name = "purchase")
-	private Purchase purchase;
+	
+	@Field(name = "cardNumber")
+	private String cardNumber;
 
 	@Field(name = "description")
 	private String description;
 
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
 	@Field(name = "transactionDate")
-	private LocalDateTime transactionDate;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+	private LocalDateTime transactionDate = LocalDateTime.now();
 	
 }
