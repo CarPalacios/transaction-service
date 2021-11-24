3178732ee6ec96b0b647583e1314d16b81243e0f
Carlos Alberto Palacios Aguado <cpalacag@emeal.nttdata.com>
2021-11-23 19:02:58 -0500

    Segundo Commit

--- src/main/java/com/nttdata/TransactionServiceApplication.java
+++ src/main/java/com/nttdata/TransactionServiceApplication.java
@@ -2,11 +2,13 @@ package com.nttdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class TransactionServiceApplication {
 
--- src/main/java/com/nttdata/controller/TransactionController.java
+++ src/main/java/com/nttdata/controller/TransactionController.java
@@ -1,14 +1,12 @@
 package com.nttdata.controller;
 
 import java.net.URI;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.nttdata.model.Transaction;
 import com.nttdata.service.ITransactionService;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -27,21 +26,15 @@ public class TransactionController {
 	private ITransactionService service;
 	
 	@GetMapping
-	public Mono<ResponseEntity<List<Transaction>>> findAll(){ 
+	public Mono<ResponseEntity<Flux<Transaction>>> findAll() {
 		
-		return service.findAll()
-				.collectList()
-				.flatMap(list -> {
-					return list.size() > 0 ? 
-							Mono.just(ResponseEntity
-									.ok()
-									.contentType(MediaType.APPLICATION_JSON)
-									.body(list)) :
-							Mono.just(ResponseEntity
-									.noContent()
-									.build());
-				});
-				
+		Flux<Transaction> transaction = service.findAll();
+		
+		return Mono.just(ResponseEntity
+				.ok()
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(transaction));							
+		
 	}
 	
 	@PostMapping
